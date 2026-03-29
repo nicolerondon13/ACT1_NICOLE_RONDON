65d5a3951365fbe08b08db7d2f1fceabfec34b00
Nicole <nicole.rondon@est.iudigital.edu.co>
2026-03-29 02:14:19 -0500

    actualizacion cuarto commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
-import Generos  from "./pages/Genero";
+import Generos from "./pages/Genero";
 import Director from "./pages/Director";
 import Productora from "./pages/Productora";
 import Tipo from "./pages/Tipo";
@@ -7,16 +9,20 @@ import Media from "./pages/Media";
 
 function App() {
   return (
-    <div className="contaniner mt-4">
+    <Router>
       <Navbar />
 
-    
-      <Generos />
-      <Director />
-      <Productora />
-      <Tipo />
-      <Media />
-    </div>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Media />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/generos" element={<Generos />} />
+          <Route path="/directores" element={<Director />} />
+          <Route path="/productoras" element={<Productora />} />
+          <Route path="/tipos" element={<Tipo />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,36 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar navbar-dark bg-dark">
       <div className="container">
-        <span className="navbar-brand">App Películas</span>
+
+        <Link className="navbar-brand" to="/">
+          🎬 App Películas
+        </Link>
+
+        <div>
+          <Link className="btn btn-outline-light me-2" to="/media">
+            Media
+          </Link>
+
+          <Link className="btn btn-outline-light me-2" to="/generos">
+            Géneros
+          </Link>
+
+          <Link className="btn btn-outline-light me-2" to="/directores">
+            Directores
+          </Link>
+
+          <Link className="btn btn-outline-light me-2" to="/productoras">
+            Productoras
+          </Link>
+
+          <Link className="btn btn-outline-light" to="/tipos">
+            Tipos
+          </Link>
+        </div>
+
       </div>
     </nav>
   );
